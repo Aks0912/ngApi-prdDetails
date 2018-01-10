d364765c62ea7f801a11b61d6cd5b1c3adbe1ee3
Aks0912 <mail2akanksha05@gmail.com>
2018-01-10 17:08:02 +0530

    testing

--- server.js
+++ server.js
@@ -1,31 +1,25 @@
-
 /***************Fecth data using mongodb where its is realted to model***********/
 var express = require('express');
 var mongodb = require('mongodb').MongoClient;
 
 var app = express();
 
-var port = process.env.PORT||3300; 
+var port = process.env.PORT||3000; 
 var commanRouter = express.Router();
 
-commanRouter.route('/getProductDetails')
+commanRouter.route('/getProducts')
     .get(function(req,res){
-    	let prdId = req.params.ProductId;
-    	console.log(prdId);
-
-    	let query = {"ProductId": '"'+ prdId + '"'};
     	var url = 'mongodb://root:admin@ds127536.mlab.com:27536/dbsansmercy'
     	mongodb.connect(url, (err, db) => {  
 			  if (err) {
 			    return console.log(err);
 			  }
-			  db.collection('products').find(query).toArray(
+			  db.collection('products').find({}).toArray(
             	function(err,data){
 					if(err)
 					   
 					   res.status(500).send(err);
 					else
-						console.log(prdId);
 						res.setHeader('Access-Control-Allow-Origin','*')
 		    		    res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept')
 						res.json(data);
@@ -37,7 +31,6 @@ commanRouter.route('/getProductDetails')
 app.use('/api', commanRouter);
 
 app.get('/',function(req,res){
-	console.log("app.get method:....", req);
 	res.send("Working")
 });
 app.use(express.static(__dirname + '/public'));
